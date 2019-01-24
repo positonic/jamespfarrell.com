5f2587f9ef4692414676047ad2e1076c8307211f
James <jamespfarrell@gmail.com>
2019-01-24 18:18:33 +0100

    text changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <div id="home-page" class="row{{-- page-container-content--}}">
         <div class="col-md-12">
             <br/>
-            <h1>a software engineering consultant ...</h1>
+            <h1>A software engineering consultant</h1>
             <h2>... with a proven track record of working on <a href="/consulting/case-studies">successful projects</a>.</h2>
             <h2>...who has consulted with renowned <a href="/consulting/digital-agencies">Digital Agencies</a> as well as freelance work with <a href="/consulting/top-tier">top tier</a> clients.</h2>
             <h2>...with client facing experience including stakeholder management.</h2>
